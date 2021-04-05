cb6810dcfce7753638ebfd00f8d763160520291b
carlos pc <carlos pc@DESKTOP-CPVS42F>
2021-04-05 21:18:03 +0200

    descripcion : commenta servicio email por problemas de despliegue de pruebas

--- src/main/java/com/carlosrey/springboot/backend/apirest/models/services/impl/EmailServiceImpl.java
+++ src/main/java/com/carlosrey/springboot/backend/apirest/models/services/impl/EmailServiceImpl.java
@@ -1,81 +1,81 @@
 package com.carlosrey.springboot.backend.apirest.models.services.impl;
-
-import javax.mail.internet.MimeMessage;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
-
-import com.carlosrey.springboot.backend.apirest.models.dao.INotificationDao;
-import com.carlosrey.springboot.backend.apirest.models.entity.EmailTemplate;
-import com.carlosrey.springboot.backend.apirest.models.entity.Notificacion;
-import com.carlosrey.springboot.backend.apirest.models.services.IEmailService;
-
-/**
- * @author Carlos Rey Silva 
- * https://github.com/carlos1811
- */
-
-@Service
-public class EmailServiceImpl implements IEmailService{
-	
-	private static final Logger logger = LoggerFactory.getLogger(EmailServiceImpl.class);
-	
-	@Autowired
-	private JavaMailSender javaMailSender;
-	
-	@Autowired
-	private INotificationDao iNotificationDao;
-	
-	public void processSendEmail(String receiver, String templateCode, String currentName)
-			throws Exception {
-		
-		final EmailTemplate emailTemplate = findTemplateAndReplace(templateCode, currentName);
-		
-		this.sendEmail(receiver, emailTemplate.getSubject(), emailTemplate.getTemplate());
-
-	}
-	
-	private void sendEmail(final String receiver, final String subject, final String template) throws Exception{
-		
-		final MimeMessage email = javaMailSender.createMimeMessage();
-		final MimeMessageHelper content;
-		
-		try {
-			
-			content = new MimeMessageHelper(email, true);
-			content.setSubject(subject);
-			content.setTo(receiver);
-			content.setText(template,true);
-						
-		}catch (Exception e) {
-			
-			logger.error("ERROR",e);
-			throw new Exception("");
-		}
-		
-		javaMailSender.send(email);
-		
-	}
-	
-	private EmailTemplate findTemplateAndReplace ( final String TemplateType, final String currentName) throws Exception
-	{
-		
-		
-		
-		final Notificacion notificacion = iNotificationDao.findByTemplateType(TemplateType);
-		
-		
-		
-		final EmailTemplate emailTemplate = new EmailTemplate();
-		emailTemplate.setSubject(notificacion.getTemplateType());
-		emailTemplate.setTemplate(notificacion.getTemplate()
-				.replaceAll("\\{"+"name"+"\\}",currentName));
-		
-		return emailTemplate;
-	}
-
-}
+//
+//import javax.mail.internet.MimeMessage;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.MimeMessageHelper;
+//import org.springframework.stereotype.Service;
+//
+//import com.carlosrey.springboot.backend.apirest.models.dao.INotificationDao;
+//import com.carlosrey.springboot.backend.apirest.models.entity.EmailTemplate;
+//import com.carlosrey.springboot.backend.apirest.models.entity.Notificacion;
+//import com.carlosrey.springboot.backend.apirest.models.services.IEmailService;
+//
+///**
+// * @author Carlos Rey Silva 
+// * https://github.com/carlos1811
+// */
+//
+//@Service
+//public class EmailServiceImpl implements IEmailService{
+//	
+//	private static final Logger logger = LoggerFactory.getLogger(EmailServiceImpl.class);
+//	
+//	@Autowired
+//	private JavaMailSender javaMailSender;
+//	
+//	@Autowired
+//	private INotificationDao iNotificationDao;
+//	
+//	public void processSendEmail(String receiver, String templateCode, String currentName)
+//			throws Exception {
+//		
+//		final EmailTemplate emailTemplate = findTemplateAndReplace(templateCode, currentName);
+//		
+//		this.sendEmail(receiver, emailTemplate.getSubject(), emailTemplate.getTemplate());
+//
+//	}
+//	
+//	private void sendEmail(final String receiver, final String subject, final String template) throws Exception{
+//		
+//		final MimeMessage email = javaMailSender.createMimeMessage();
+//		final MimeMessageHelper content;
+//		
+//		try {
+//			
+//			content = new MimeMessageHelper(email, true);
+//			content.setSubject(subject);
+//			content.setTo(receiver);
+//			content.setText(template,true);
+//						
+//		}catch (Exception e) {
+//			
+//			logger.error("ERROR",e);
+//			throw new Exception("");
+//		}
+//		
+//		javaMailSender.send(email);
+//		
+//	}
+//	
+//	private EmailTemplate findTemplateAndReplace ( final String TemplateType, final String currentName) throws Exception
+//	{
+//		
+//		
+//		
+//		final Notificacion notificacion = iNotificationDao.findByTemplateType(TemplateType);
+//		
+//		
+//		
+//		final EmailTemplate emailTemplate = new EmailTemplate();
+//		emailTemplate.setSubject(notificacion.getTemplateType());
+//		emailTemplate.setTemplate(notificacion.getTemplate()
+//				.replaceAll("\\{"+"name"+"\\}",currentName));
+//		
+//		return emailTemplate;
+//	}
+//
+//}
